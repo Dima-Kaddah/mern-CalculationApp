6bb1f28d020c3ccfc85d76c994899dcdd1c57b99
Dima <dimakaddah@gmail.com>
2020-10-20 23:09:40 +0200

    add build file

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const userRoute = require('./routes/userRouts');
 //middleware
 app.use(morgan('dev')); //give route in console//good for development
 app.use(bodyParser.json());
+app.use(favicon(__dirname + '/client/public/favicon.ico'));
 
 //middleware to allow connect between 3000,5000 servers// its open to any domain//set more headers//for CORS cross origin resorse shearing error... that the requset must be from same sever .... this code bellow make the access
 app.use((req, res, next) => {
@@ -38,8 +39,7 @@ app.use('/api', userRoute);
 //     res.sendFile(path.resolve(__dirname, '../quiz-app-frontend', 'build', 'index.html'));
 //   });
 // }
-app.use(favicon(__dirname + '/client/public/favicon.ico'));
-app.user(express.static(path.join(__dirname, '/client/build')));
+// app.user(express.static(path.join(__dirname, '/client/build')));
 app.get('/*', function (req, res) {
   res.sendFile(path.resolve(__dirname, './client', 'build', 'index.html'));
 });
