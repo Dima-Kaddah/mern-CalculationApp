fcda27c4154c09fdedbaaf1fafb4ef94f715d262
Dima <dimakaddah@gmail.com>
2020-10-22 07:03:36 +0200

    add role in model for lrvels

--- models/Questions.js
+++ models/Questions.js
@@ -6,6 +6,7 @@ const questionSchema = new mongoose.Schema(
     index: { type: Number, required: true, },
     question: { type: String, required: true, maxlength: 30 },
     answer: { type: String, required: true },
+    role: { type: String, required: true },
     triesCount: { type: Number, default: '1' },
     rightCount: { type: Number, default: '1' }
   },
