f7fab6952186cafed1cb764e284980651bbeb837
Dima <dimakaddah@gmail.com>
2020-10-22 07:01:45 +0200

    add play page with levels

--- client/src/questions/pages/PlayStartPage.js
+++ client/src/questions/pages/PlayStartPage.js
@@ -1,27 +1,44 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useContext } from 'react';
 import { Helmet } from 'react-helmet';
 import { Link } from 'react-router-dom';
 import LightBulbOn from 'mdi-react/LightbulbOnOutlineIcon';
-import mathGif from '../../images/giphy/math2.gif';
 import './PlayStartPage.css';
+import { LevelContext } from '../../shared/Level-context';
 
-const PlayStartPage = () => (
-  <Fragment>
-    <Helmet><title>Math-Quiz App - Home</title></Helmet>
-    <section>
-      <div style={{ textAlign: 'center' }}>
-        <LightBulbOn color="orange" size={'8rem'} />
-      </div>
-      <h1>Math Quiz App</h1>
-      <div style={{ textAlign: 'center' }}>
-        <img src={mathGif} alt="light" />
-      </div>
-      <div className='start-btn-container'>
-        <Link to='/QuestionCard' className='start-btn btn'>Start</Link>
-      </div>
-    </section>
-  </Fragment >
-);
+const PlayStartPage = () => {
+  const { gameLevel, setGameLevel } = useContext(LevelContext);
+
+  const [level, setLevel] = useState('');
+
+  const onChangeHandler = (e) => {
+    setLevel(e.target.value);
+    setGameLevel(e.target.value);
+
+  };
+
+  const fetchLevel = (e) => {
+    e.preventDefault();
+    console.log(gameLevel);
+  };
+
+  return (
+    <Fragment>
+      <Helmet><title>Math-Quiz App - play/level</title></Helmet>
+      <section>
+        <h1>Select<LightBulbOn color="orange" size={'8rem'} /> Level</h1>
+        <form onSubmit={fetchLevel}>
+          <select className='select' onChange={onChangeHandler}>
+            <option className='select' value="EASY">EASY</option>
+            <option className='select' value="NORMAL">NORMAL</option>
+            <option className='select' value="HARD">HARD</option>
+          </select>
+          <div className='start-btn-container'>
+            <Link to='/QuestionCard' className='start-btn btn'> {level} PLAY </Link>
+          </div>
+        </form>
+      </section>
+    </Fragment >);
+};
 
 
 export default PlayStartPage;
