5cc395dd9622f878c6454fd13420fd4cd7fb69ed
Dima <dimakaddah@gmail.com>
2020-10-26 23:00:32 +0100

    change answer input type

--- client/src/questions/components/Form.js
+++ client/src/questions/components/Form.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Form = ({ handleSubmit, answer, onChange }) => {
   return (
     <form onSubmit={handleSubmit}>
-      <input type="number" placeholder='0' value={answer} onChange={onChange} autoFocus />
+      <input type="text" placeholder='0' value={answer} onChange={onChange} autoFocus />
       <div className='play-btn-container'>
         <button type='submit' className='play-btn btn'>check</button>
       </div>
