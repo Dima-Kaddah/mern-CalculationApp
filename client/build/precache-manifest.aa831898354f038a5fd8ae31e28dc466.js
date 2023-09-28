e38e4797f836a22ac1bc109e7b7993619c7a64f8
Dima Kaddah <dimakaddah@gmail.com>
2023-09-28 17:14:37 +0200

    add build

---
+++ client/build/precache-manifest.aa831898354f038a5fd8ae31e28dc466.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59c8966d706fa6f194b9b51884010e39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "796690a3b4dd7266ff13",
+    "url": "/static/css/3.46579d70.chunk.css"
+  },
+  {
+    "revision": "ca34bceb3f810a157b05",
+    "url": "/static/css/4.46579d70.chunk.css"
+  },
+  {
+    "revision": "0acccbd08c729b9bf2f2",
+    "url": "/static/css/5.46579d70.chunk.css"
+  },
+  {
+    "revision": "3b8fd24fc18ada5b41a0",
+    "url": "/static/css/main.818b72b6.chunk.css"
+  },
+  {
+    "revision": "3d473c562c4ec4423e4a",
+    "url": "/static/js/2.e8cb466b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.e8cb466b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "796690a3b4dd7266ff13",
+    "url": "/static/js/3.4c835b13.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/3.4c835b13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca34bceb3f810a157b05",
+    "url": "/static/js/4.12856460.chunk.js"
+  },
+  {
+    "revision": "0acccbd08c729b9bf2f2",
+    "url": "/static/js/5.def268bc.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/5.def268bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b8fd24fc18ada5b41a0",
+    "url": "/static/js/main.005f20fa.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.005f20fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d8359faa0fbbb8d3d0e",
+    "url": "/static/js/runtime-main.10d1cf3f.js"
+  },
+  {
+    "revision": "3f38b18bc7111721a1233a3c8c1530ac",
+    "url": "/static/media/804211.3f38b18b.jpg"
+  },
+  {
+    "revision": "a214553ce48081cf9c166c0cf4df05e8",
+    "url": "/static/media/RightAnswer.a214553c.gif"
+  },
+  {
+    "revision": "5e9355f48fcc034c3394ec6d0daf9c3b",
+    "url": "/static/media/error.5e9355f4.gif"
+  },
+  {
+    "revision": "1d98a5293b88a4ab2c046cf893baaa12",
+    "url": "/static/media/loading.1d98a529.gif"
+  },
+  {
+    "revision": "44c7f4894693f9a4a3b9ff51772f4953",
+    "url": "/static/media/math2.44c7f489.gif"
+  },
+  {
+    "revision": "d744a35cd560ad36e769b6df63ca3f7f",
+    "url": "/static/media/tryAgain.d744a35c.gif"
+  },
+  {
+    "revision": "61f59956391829b68b072757bfb2e57c",
+    "url": "/static/media/youLose.61f59956.gif"
+  },
+  {
+    "revision": "ddc8161816f188e301c93f12a544d0b7",
+    "url": "/static/media/youWin.ddc81618.gif"
+  }
+]);
